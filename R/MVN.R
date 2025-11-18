3ca1e252a371df534f932b2670d27d4bcb33b147
thistle-sz <hg81@st-andrews.ac.uk>
2025-11-18 12:52:31 +0000

    Reformatted section to match with standard package format and to allow function to be called properly by the parent function.  Fixed some minor spelling mistakes in the comments.

--- R/MVN.R
+++ R/MVN.R
@@ -1,36 +1,46 @@
-# Need to input the grad and hessian functions from numDeriv
+# -----------------------------------
+# Helper Function: Tolerance Checker
+# -----------------------------------
+#' @description
+#' Computes the ratio of the Euclidean norm of 2 vectors
+#'
+#' @param delta step value for each parameter
+#' @param theta estimate of the minimised values
+#'
+#' @returns the ratio of the Euclidean norm of the inputted vectors
+#' @export
 
-# Helper functions:
-
-# Computes the ratio of the Euclidean norm of 2 vectors
-# INPUTS:
-# delta: step value for each parameter
-# theta: estimate of minimised values
 Norm_Ratio <- function(delta, theta){
   return((sqrt(sum(delta^2))) / (sqrt(sum(theta^2))))
 }
 
-# INPUTS:
-# f: function to be minimised
-# inits: vector of initial values to search through
-# data: (optional) dataframe with appropriate number of columns for number of
-# variables being optimised
-# minimum: seach for minimum or maximum (takes values TRUE or FALSE)
-# tol: tolerance level
-# maxit: max number of iterations run before stopping
-# method: MVN, to call the parent function
-# gradfn:(optional) gradient function of f, uses finite differencing otherwise
-# hessfn: (optional) hessian function of f, uses finite differencing otherwise
-# jacobfn: NULL variable, included to match parent function inputs
-
+# -----------------------------------
+# Multivariate Newton's Method (MVN)
+# -----------------------------------
+#' @description
+#' Performs multivariate optimisation using Newton's method
+#'
+#' @param f function to be minimised
+#' @param inits vector of initial values to be minimised
+#' @param data (optional) daatframe with appropriate number of columns for number of variables being optimised
+#' @param minimum search for minimum or maximum (takes TRUE if minimum search, FALSE if maximum search)
+#' @param tol tolerance level
+#' @param maxit maximum number of iterations run before stopping
+#' @param method identifier, takes "MVN" only to allow the parent function to call on it
+#' @param gradfn (optional) gradient function of f, uses finite differencing otherwise
+#' @param hessfn (optional) hessian function of f, uses finite differencing otherwise
+#' @param jacobfn NULL variable, included to match parent function inputs
+#'
+#' @returns A list containing the optimised estimate, the function evaluated at said estimate, the gradient of the function at the time, the tolerance level, whether the optimisation converged and the number of iterations ran.
+#' @export
 
 MVN <- function(f, inits, data = NULL, minimum = TRUE, tol, maxit,
                 method = "MVN", gradfn = NULL, hessfn = NULL, jacobfn = NULL){
-  # If searching fro a maximum, work with the negative of the function, as Newton searches for minimums
+  # If searching for a maximum, work with the negative of the function, as Newton searches for minimums
   if (minimum == FALSE) {
     f <- -f}
 
-  # If not provided with analytic fucntions, use numderiv to estimate
+  # If not provided with analytic functions, use numDeriv to estimate
   if (is.null(gradfn)){
     gradfn <- function(theta, data) {grad(f, theta, data = data)}
   }
@@ -56,7 +66,7 @@ MVN <- function(f, inits, data = NULL, minimum = TRUE, tol, maxit,
     theta <- theta + delta
   }
   # Check convergence
-  # 0: tolerance reached, 1: tolerance reached, 2: max. iterationa reached
+  # 0: tolerance reached, 1: tolerance not reached, 2: max. iterations reached
   if (NormRatio(delta, theta) < tol) {
     conv <- 0
   } else if (niter > maxit) {
