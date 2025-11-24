1087ad815ba4784280431a2d15d9fad97fb59653
CharlieBeer <cbeer264@gmail.com>
2025-11-24 12:14:27 +0000

    Created test file & filled BS & GS parts

---
+++ R/Test File.R
@@ -0,0 +1,20 @@
+#Test File
+
+#BS
+test_f<-function(x){
+  return (x^5-3*x^4+12*x^3-5*x^2-3)
+}
+test_inits<-c(0.1,5)
+funoptim(test_f,test_inits,method="BS")
+
+#GN
+
+#GS
+test_f<-function(x){
+  return ((x-0.25)*(x-0.6)*(x-5)*(x-3))
+}
+test_inits<-c(0)
+GS(test_f,test_inits,minimum=TRUE,method="GS")
+#MVN
+
+#UVN
